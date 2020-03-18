9d3365dc8fc3e1acea166404a293a339cd5c81df
Bianwy <bianwy025@qq.com>
2020-03-18 19:19:58 +0800

    '实现多页面的配置和分离。'

--- app/renderer/src/main/vue.config.js
+++ app/renderer/src/main/vue.config.js
@@ -1,12 +1,31 @@
 module.exports = {
   pages: {
-    index: 'src/main.js',
-    subpage: 'src/subpage/main.js'
+    index: {
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+      title: 'Index Page'
+    },
+    subpage: {
+      entry: 'src/subpage/main.js',
+      template: 'public/subpage.html',
+      filename: 'subpage.html',
+      title: 'Second Page'
+    }
   },
   pluginOptions: {
     electronBuilder: {
       outputDir: 'dist', // default is dis_electron
-      mainProcessFile: '../../../main/index.js'
+      mainProcessFile: '../../../main/index.js',
+      chainWebpackRendererProcess: config => {
+        // Chain webpack config for electron renderer process only
+        // The following example will set IS_ELECTRON to true in your app
+        config.plugin('define').tap(args => {
+          console.log('args', args);
+          args[0]['IS_ELECTRON'] = true;
+          return args;
+        });
+      }
     }
   }
 };
