720a7670bed74c970c23645f2011a2091fc0ae70
Bianwy <bianwy025@qq.com>
2020-03-18 14:47:15 +0800

    '实现了主进程入口文件的迁移。'

--- app/renderer/src/main/vue.config.js
+++ app/renderer/src/main/vue.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   pluginOptions: {
     electronBuilder: {
-      outputDir: 'dist' // default is dis_electron
+      outputDir: 'dist', // default is dis_electron
+      mainProcessFile: '../../../main/index.js'
     }
   }
 };
